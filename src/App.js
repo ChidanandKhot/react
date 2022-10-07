6124d389393e3dd75309a1c5641dc4dd4f92596f
chidanandkhot <chidukhot@gmail.com>
2022-10-07 18:41:44 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function AppCare() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+       The Header page - React
+
+      </header>
+    </div>
+  );
+}
+
+export default AppCare;
